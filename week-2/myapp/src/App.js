8d020030e571d03a4989a931bd175eae71db627b
Shiwei Huang <shiwei.julia.huang@gmail.com>
2020-10-13 18:57:10 -0400

    continued

--- week-2/myapp/src/App.js
+++ week-2/myapp/src/App.js
@@ -1,132 +1,107 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React, Hello World!
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function addTask(name) {
+  alert(name);
+}
+
+// const [tasks, setTasks] = useState(props.tasks);
 function App(props) {
+  const taskList = props.tasks.map(task => (
+    <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+      />
+    )
+  );
+
   return (
     <div className="todoapp stack-large">
-      <h1>Todo List</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What's on your mind?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form addTask={addTask} />
+      {/* <form onSubmit={handleSubmit}></form> */}
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">3 tasks remaining</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo name = "Eat" completed={true} id = "todo-0" />
-        <Todo name = "Sleep" completed={true} id = "todo-1" />
-        <Todo name = "Repeat" completed={true} id = "todo-2" />
-        {/* <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label className="todo-label" htmlFor="todo-0">
-              Eat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Sleep
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Sleep</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Sleep</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li> */}
+        {taskList}
       </ul>
     </div>
   );
 }
 
-
 export default App;
+
+//   return (
+//     <div className="todoapp stack-large">
+//       <h1>Todo List</h1>
+//       <form>
+//         <h2 className="label-wrapper">
+//           <label htmlFor="new-todo-input" className="label__lg">
+//             What's on your mind?
+//           </label>
+//         </h2>
+//         <input
+//           type="text"
+//           id="new-todo-input"
+//           className="input input__lg"
+//           name="text"
+//           autoComplete="off"
+//         />
+//         <button type="submit" className="btn btn__primary btn__lg">
+//           Add
+//         </button>
+//       </form>
+//       <div className="filters btn-group stack-exception">
+//         <button type="button" className="btn toggle-btn" aria-pressed="true">
+//           <span className="visually-hidden">Show </span>
+//           <span>all</span>
+//           <span className="visually-hidden"> tasks</span>
+//         </button>
+//         <button type="button" className="btn toggle-btn" aria-pressed="false">
+//           <span className="visually-hidden">Show </span>
+//           <span>Active</span>
+//           <span className="visually-hidden"> tasks</span>
+//         </button>
+//         <button type="button" className="btn toggle-btn" aria-pressed="false">
+//           <span className="visually-hidden">Show </span>
+//           <span>Completed</span>
+//           <span className="visually-hidden"> tasks</span>
+//         </button>
+//       </div>
+//       <h2 id="list-heading">
+//         3 tasks remaining
+//       </h2>
+//       <ul
+//         role="list"
+//         className="todo-list stack-large stack-exception"
+//         aria-labelledby="list-heading"
+//       >
+//         {taskList}
+        
+//         {/* <Todo name = "Eat" completed={true} id = "todo-0" />
+//         <Todo name = "Sleep" completed={true} id = "todo-1" />
+//         <Todo name = "Repeat" completed={true} id = "todo-2" /> */}
+    
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+// export default App;
 
 // Task:
 // Create a todo applicagtion where people can add tasks under the task bar
